6237ed75782cfee8d07837b31befbdd3708ea6f3
Finley Davis <hfdavis@utexas.edu>
2025-05-03 13:44:11 -0500

    added phreeqc files

--- stevensite_calc.py
+++ stevensite_calc.py
@@ -1,4 +1,4 @@
-#stevensite solubility product
+#stevensite solubility product is below from a given paper
 
 #Given:
 #logK = 25.45 @ 25C
@@ -14,9 +14,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-#load data
+#load data, original df
 df = pd.read_csv('/Users/finleydavis/Desktop/Spring 25 Courses/Aqueous Geochem/Final Paper/Data/metadata_ioncomp.csv')
-
 
 #extracting columns
 Mg = df['Magnesium (Mg)'].to_numpy()
@@ -25,112 +24,7 @@ H4SiO4 = df['Silicon (Si)'].to_numpy()  # ssuming silicon column represents H4Si
 df['Date'] = pd.to_datetime(df['Sample Date'], format='%m/%d/%y')
 temp = df['Temperature (C)'].to_numpy()
 
-
-#NOTE: Concentrations are in mmol/L
-
-#fresh
-def log_Ksp_stevensite_2014_SW():
-    results = []
-    for i in range(48, 62):  # specific rows for BL surface water
-        pH = df['pH'].iloc[i]
-        Mg = df['Magnesium (Mg)'].iloc[i]
-        Ca = df['Calcium (Ca)'].iloc[i]
-        Si = df['Silicon (Si)'].iloc[i]  # assuming Si = H4SiO4
-        temp = df['Temperature (C)'].iloc[i]
-        date = df['Date'].iloc[i]
-
-        H = 10 ** (-pH)
-        Ksp = (Si ** 4) * (Mg ** 2.9) * (Ca ** 0.1) / (H ** 6)
-        log_ksp = np.log10(Ksp)
-
-        results.append({'Date': date, 'Temperature (C)': temp, 'log_Ksp': log_ksp, 'pH': pH})
-
-    return pd.DataFrame(results)
-
-
-    #output this into csv file
-
-#saline
-def log_Ksp_stevensite_2014_PW_Inactive():
-    results = []
-
-    for i in range(5, 21):
-        pH = df['pH'].iloc[i]
-        Mg = df['Magnesium (Mg)'].iloc[i]
-        Ca = df['Calcium (Ca)'].iloc[i]
-        Si = df['Silicon (Si)'].iloc[i]
-        temp = df['Temperature (C)'].iloc[i]
-        H = 10 ** -pH
-
-        Ksp = (Si ** 4) * (Mg ** 2.9) * (Ca ** 0.1) / (H ** 6)
-        log_ksp = np.log10(Ksp)
-
-        date = df['Date'].iloc[i]
-        results.append({'Date': date, 'Temperature (C)': temp, 'log_Ksp': log_ksp, 'pH': pH})
-
-    return pd.DataFrame(results)
-
-#saline
-def log_Ksp_stevensite_2014_PW_Active():
-    results = []
-
-    for i in range(23, 43):
-        pH = df['pH'].iloc[i]
-        Mg = df['Magnesium (Mg)'].iloc[i]
-        Ca = df['Calcium (Ca)'].iloc[i]
-        Si = df['Silicon (Si)'].iloc[i]
-        temp = df['Temperature (C)'].iloc[i]
-        H = 10 ** -pH
-
-        Ksp = (Si ** 4) * (Mg ** 2.9) * (Ca ** 0.1) / (H ** 6)
-        log_ksp = np.log10(Ksp)
-
-        date = df['Date'].iloc[i]
-        results.append({'Date': date, 'Temperature (C)': temp, 'log_Ksp': log_ksp, 'pH': pH})
-
-    return pd.DataFrame(results)
-
-#brackish
-def log_Ksp_stevensite_2014_GW():
-    results = []
-
-    for i in range(3, 5):
-        pH = df['pH'].iloc[i]
-        Mg = df['Magnesium (Mg)'].iloc[i]
-        Ca = df['Calcium (Ca)'].iloc[i]
-        Si = df['Silicon (Si)'].iloc[i]
-        temp = df['Temperature (C)'].iloc[i]
-        H = 10 ** -pH
-
-        Ksp = (Si ** 4) * (Mg ** 2.9) * (Ca ** 0.1) / (H ** 6)
-        log_ksp = np.log10(Ksp)
-
-        date = df['Date'].iloc[i]
-        results.append({'Date': date, 'Temperature (C)': temp, 'log_Ksp': log_ksp, 'pH': pH})
-
-    return pd.DataFrame(results)
-
-#run functions
-def run_above_functions():
-    SW = log_Ksp_stevensite_2014_SW()
-    SW['Sample_Type'] = 'Surface Water'
-
-    PW_Inactive = log_Ksp_stevensite_2014_PW_Inactive()
-    PW_Inactive['Sample_Type'] = 'Inactive Pore Water'
-
-    PW_Active = log_Ksp_stevensite_2014_PW_Active()
-    PW_Active['Sample_Type'] = 'Active Pore Water'
-
-    GW = log_Ksp_stevensite_2014_GW()
-    GW['Sample_Type'] = 'Groundwater'
-
-    #combine into one DataFrame
-    combined_df = pd.concat([SW, PW_Inactive, PW_Active, GW], ignore_index=True)
-
-    #save to CSV
-    combined_df.to_csv('/Users/finleydavis/Desktop/Spring 25 Courses/Aqueous Geochem/Final Paper/Data/stevensite_combined_logKsp.csv', index=False)
-
-#scatter plot, comparing logKsp values to temperature
+#scatter plot, comparing SI values
 def plot_SI_scatter():
     df = pd.read_csv('/Users/finleydavis/Desktop/Spring 25 Courses/Aqueous Geochem/Final Paper/github/CSV Data/Stevensite_SI_phrqc.csv')
 
@@ -170,14 +64,14 @@ def plot_SI_scatter():
     plt.tight_layout()
     plt.show()
 
-#box plot, comparing logKsp values to sample type
+#box plot, comparing SI values to sample type
 def plot_SI_boxplot():
     df = pd.read_csv('/Users/finleydavis/Desktop/Spring 25 Courses/Aqueous Geochem/Final Paper'
     '/github/CSV Data/Stevensite_SI_phrqc.csv')
 
     df['Sample Type'] = df['Location ID'].str.strip()
 
-    # Filter out rows where 'Location ID' is 'Undisclosed Porewater'
+    #filtering out rows where 'Location ID' is 'Undisclosed Porewater'
     df = df[df['Sample Type'] != 'Undisclosed Porewater']
 
     plt.figure(figsize=(10, 6))
@@ -212,6 +106,5 @@ def plot_SI_boxplot():
     plt.tight_layout()
     plt.show()
 
-
 #plot_SI_scatter()
-plot_SI_boxplot()
+#plot_SI_boxplot()
